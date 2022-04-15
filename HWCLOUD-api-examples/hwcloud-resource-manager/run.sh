18a0efb62915b8122e95348ed5448128f9f44f0d
CentaurusM <inboxmotor@gmail.com>
2022-04-15 16:54:50 +0800

    Delete HWCLOUD-api-examples directory

--- HWCLOUD-api-examples/hwcloud-resource-manager/run.sh
+++
@@ -1,2 +0,0 @@
-pkill -f hwcloud_
-nohup python -u -W ignore  hwcloud_resouce_manager.py >> monitor.log 2>&1 &
--- HWCLOUD-api-examples/mount_sfs_windows.cmd
+++
@@ -1,1 +0,0 @@
-net use X: \\sfs-nas1.cn-north-1.myhuaweicloud.com\share-a7e8d5d1 qinxuan@123 /user:qinxuan_mafuda
